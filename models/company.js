6a436120b64e921522bb9ed0eb45c3a660b4eab9
Inventry - RZ <Inventry@Richards-MBP.lan>
2018-01-29 16:15:44 -0600

    push-updates

---
+++ models/company.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  var Company = sequelize.define('Company', {
+    name: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Company;
+};
